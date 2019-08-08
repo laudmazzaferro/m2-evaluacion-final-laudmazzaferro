a82ecc4f17280ebc21fc33aa34e60e649169a6c1
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-08-08 11:08:48 +0200

    add default image when imageSerie = null

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,24 +4,39 @@ const searchBtn = document.querySelector('.search-btn');
 const inputText = document.querySelector('.field__fill-series');
 const api = 'http://api.tvmaze.com/search/shows?q=';
 
-
+function favSeries(event){
+console.log(event.currentTarget);
+}
 function seriesSearch(){
   const inputSerie =inputText.value;
 
   fetch(api + inputSerie)
     .then(response => response.json())
     .then(data => {
-      //let itemlist='';
+      seriesList.innerHTML='';
       for (const item of data){
-        console.log(item.show.name);
-        console.log(item.show.url);
-        seriesList.innerHTML += `
-        <li>
-        <img src="${item.show.image.medium}">
-        <h3>${item.show.name}</h3>
+        if (item.show.image === null){
+          seriesList.innerHTML += `
+        <li class="list-item">
+        <img class="img-list" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV">
+        <h3 class="name-serie">${item.show.name}</h3>
         </li>`;
+
+        }else {
+          seriesList.innerHTML += `
+        <li class="list-item">
+        <img class="img-list" src="${item.show.image.medium}">
+        <h3 class="name-serie">${item.show.name}</h3>
+        </li>`;
+        }
       }
-      //seriesList.innerHTML=itemList;
+      const itemList =document.querySelectorAll('.list-item');
+
+      for (let i=0;i<itemList.length;i++) {
+        const nameList = document.querySelector('.name-serie');
+        const imgList = document.querySelector('.img-list');
+        itemList.addEventListener('click', favSeries);
+      }
     });
 
 }
