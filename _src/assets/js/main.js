fad57a789d7ba9f5cb1f3fa4f6bcd5d6ce82c0d1
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-08-08 09:48:38 +0200

    add const and eventlistener to main.js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,14 @@
 'use strict';
+const seriesList = document.querySelector('.series-list');
+const searchBtn = document.querySelector('.search-btn');
+const inputText = document.querySelector('.field__fill-series');
 
-console.log('>> Ready :)');
+
+
+function seriesSearch(){
+  const inputSerie =inputText.value;
+  console.log(inputSerie);
+}
+
+
+searchBtn.addEventListener('click',seriesSearch);
