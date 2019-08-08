c83f77796b0ceeef6d5481acde95f153b7e03644
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-08-08 13:29:19 +0200

    create fav-list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,10 +3,45 @@ const seriesList = document.querySelector('.series-list');
 const searchBtn = document.querySelector('.search-btn');
 const inputText = document.querySelector('.field__fill-series');
 const api = 'http://api.tvmaze.com/search/shows?q=';
+let favItem = document.querySelector('.fav-list');
+let favs=[];
 
 function favSeries(event){
-console.log(event.currentTarget);
+
+  const item = event.currentTarget;
+  const idList =event.currentTarget.id;
+  const nameList = event.currentTarget.querySelector('.name-serie').innerHTML;
+  const imgList = event.currentTarget.querySelector('.img-list').src;
+  item.classList.toggle('serie-fav');
+  const object ={id:idList,name:nameList,img:imgList};
+
+  if (item.classList.contains('serie-fav')) {
+    if (favs.includes(object) === false) {
+      favs.push(object);
+    }
+  } else {
+    let index = -1;
+    for(let i = 0; i < favs.length; i++) {
+      if (favs[i].name === nameList) {
+        index = i;
+        break;
+      }
+    }
+
+    if (index > -1) {
+      favs.splice(index, 1);
+    }
+  }
+  favItem.innerHTML='';
+  for (const item of favs){
+    favItem.innerHTML +=`<li>
+    <img src="${item.img}">
+    <h3>${item.name}</h3>
+    </li>`;
+  }
+  console.log(favs);
 }
+
 function seriesSearch(){
   const inputSerie =inputText.value;
 
@@ -16,15 +51,16 @@ function seriesSearch(){
       seriesList.innerHTML='';
       for (const item of data){
         if (item.show.image === null){
+          console.log(item.show.id);
           seriesList.innerHTML += `
-        <li class="list-item">
+        <li class="list-item" id:"${item.show.id}">
         <img class="img-list" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV">
         <h3 class="name-serie">${item.show.name}</h3>
         </li>`;
 
         }else {
           seriesList.innerHTML += `
-        <li class="list-item">
+        <li class="list-item" id:"${item.show.id}">
         <img class="img-list" src="${item.show.image.medium}">
         <h3 class="name-serie">${item.show.name}</h3>
         </li>`;
@@ -33,9 +69,7 @@ function seriesSearch(){
       const itemList =document.querySelectorAll('.list-item');
 
       for (let i=0;i<itemList.length;i++) {
-        const nameList = document.querySelector('.name-serie');
-        const imgList = document.querySelector('.img-list');
-        itemList.addEventListener('click', favSeries);
+        itemList[i].addEventListener('click', favSeries);
       }
     });
 
