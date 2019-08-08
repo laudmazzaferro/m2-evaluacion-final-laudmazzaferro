54bd09692a4182806e9082685996bc6843231b71
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-08-08 10:21:42 +0200

    add list series

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,12 +2,28 @@
 const seriesList = document.querySelector('.series-list');
 const searchBtn = document.querySelector('.search-btn');
 const inputText = document.querySelector('.field__fill-series');
-
+const api = 'http://api.tvmaze.com/search/shows?q=';
 
 
 function seriesSearch(){
   const inputSerie =inputText.value;
-  console.log(inputSerie);
+
+  fetch(api + inputSerie)
+    .then(response => response.json())
+    .then(data => {
+      //let itemlist='';
+      for (const item of data){
+        console.log(item.show.name);
+        console.log(item.show.url);
+        seriesList.innerHTML += `
+        <li>
+        <img src="${item.show.image.medium}">
+        <h3>${item.show.name}</h3>
+        </li>`;
+      }
+      //seriesList.innerHTML=itemList;
+    });
+
 }
 
 
